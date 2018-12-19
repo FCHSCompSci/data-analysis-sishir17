d8cf29698fa865967a2ef360e57d4b97ed3172e0
sphuy01 <sishir.phuyal@stu.jefferson.kyschools.us>
2018-12-19 08:31:00 -0500

    yyyyyyyyyyyyyy

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import csv
 from matplotlib import pyplot as plt
-
+x=[]
+y=[]
 filename = 'Data.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     for index, column_header in enumerate(header_row):
         print(index, column_header)
-    highs = []
     for row in reader:
-
-        highs.append(row[1])
-
-    print(highs)
+        y.append(row[4])
+        x.append(row[1])
 
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
+plt.plot(y, c='red')
 
 plt.title("Premier Leauge Match Dates", fontsize=24)
 plt.xlabel('', fontsize=16)
-plt.ylabel("Dates (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 plt.show()
+
